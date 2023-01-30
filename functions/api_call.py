b90de9d1ac9654b71396a482ded8298ab586da68
hlopik <evg@it-solution.ru>
2023-01-30 16:55:17 +0300

    q

---
+++ functions/api_call.py
@@ -0,0 +1,290 @@
+# -*- coding: utf-8 -*-
+
+from pprint import pformat
+from urllib.parse import urlparse
+
+import requests
+import time
+
+import urllib
+
+import six
+
+
+# Таймаут запроса по-умолчанию:
+# 1 минута, потому что наш nginx все равно отрубает обработчики после 1 минуты,
+# если все-таки нужно выполнить ожидаемо очень долгий запрос,
+# например в кроне, можно явно передать большее значение или даже None.
+DEFAULT_TIMEOUT = 60
+
+
+class ConnectionToBitrixError(Exception):
+    pass
+
+
+@six.python_2_unicode_compatible
+class BitrixTimeout(Exception):
+    def __init__(self, requests_timeout, timeout):
+        self.request_timeout = requests_timeout
+        self.timeout = timeout
+
+    def __str__(self):
+        return '[{self.timeout} sec.] ' \
+               'requests_timeout={self.request_timeout!r} ' \
+               'request={self.request_timeout.request!r}'.format(self=self)
+
+    def __repr__(self):
+        rv = '<BitrixTimeout {!s}>'.format(self)
+        if six.PY2:
+            return rv.encode('utf8')
+        return rv
+
+
+class RawStringParam:
+    # Параметр, к которому не нужно применять urlquote
+
+    def __init__(self, value):
+        self.value = value
+
+    __unicode__ = __str__ = lambda self: self.value
+    __repr__ = lambda self: '<RawStringParam %r>' % self.value
+
+
+def call_with_retries(url, converted_params, retry_http=False,
+                      retries_on_503=20, sleep_on_503_time=0.5,
+                      timeout=DEFAULT_TIMEOUT, files=None):
+
+    response = None
+
+    verify = True
+
+    try:
+        if False: #"/crm.item." in url or "/crm.type." in url:
+            #обход бага битиркса
+            # TODO выпилить когда починят
+            # Вроде починили
+            response = requests.post(
+                "{}?{}".format(url, converted_params.decode('utf-8')),
+                converted_params,
+                auth=getattr(settings, 'B24_HTTP_BASIC_AUTH', None),
+                timeout=timeout,
+                files=files,
+                allow_redirects=False,
+                verify=verify
+            )
+        else:
+            response = requests.post(
+                url,
+                converted_params,
+                timeout=timeout,
+                files=files,
+                allow_redirects=False,
+                verify=verify
+            )
+    except requests.exceptions.SSLError as e:
+        raise ConnectionToBitrixError()
+    except requests.Timeout as e:
+        raise BitrixTimeout(requests_timeout=e, timeout=timeout)
+    except requests.ConnectionError as e:
+        raise ConnectionToBitrixError()
+    else:
+        if response.status_code == 503:
+            if retries_on_503 > 0:
+                ilogger.debug('retry_on_503=>{}'.format(pformat(dict(
+                    retries_left=retries_on_503,
+                    url=url,
+                    sleep_on_503_time=sleep_on_503_time,
+                    response=response,
+                ))))
+                if sleep_on_503_time:
+                    time.sleep(sleep_on_503_time)
+                return call_with_retries(
+                    url=url,
+                    converted_params=converted_params,
+                    retries_on_503=retries_on_503 - 1,
+                    sleep_on_503_time=sleep_on_503_time,
+                )
+            else:
+                ilogger.warn('retry_503_exceeded=>{}'.format(pformat(dict(
+                    url=url,
+                    sleep_on_503_time=sleep_on_503_time,
+                    response=response,
+                ))))
+
+        elif response.status_code in [301, 302]:
+            location = response.headers.get('location')
+            if location:
+                old_domain = urlparse(url).netloc
+                new_domain = urlparse(location).netloc
+
+                if old_domain != new_domain:
+                    ilogger.debug('retry_on_301_302=>{}'.format(pformat(dict(
+                        old_domain=old_domain,
+                        new_domain=new_domain,
+                        url=url,
+                        location=location,
+                    ))))
+
+                    return call_with_retries(
+                        url=location,
+                        converted_params=converted_params,
+                        retries_on_503=retries_on_503,
+                        sleep_on_503_time=sleep_on_503_time,
+                        timeout=timeout,
+                        files=files,
+                    )
+
+            ilogger.warn('retry_on_301_302_failed=>{}'.format(pformat(dict(
+                url=url,
+                location=location,
+                response=response,
+            ))))
+
+    return response
+
+
+# compat
+call_with_fall_to_http = call_with_retries
+
+
+def convert_params(form_data):
+    """
+    Рекурсивно, с помощью функции recursive_traverse проходит словарь/кортеж/список,
+    превращая его в параметры, понятные битриксу.
+
+    Обычный вызов
+    >>> convert_params({'field': {'hello': 'world'}})
+    'field[hello]=world'
+
+    Вызов со списком
+    >>> convert_params([{'field': 'hello'}, {'field': 'world'}])
+    '0[field]=hello&1[field]=world'
+
+    Обычный вызов
+    >>> convert_params({'auth': 123, 'field': {'hello': 'world'}})
+    'auth=123&field[hello]=world'
+
+    Экранирование ключей
+    >>> convert_params({'FILTER': {'>=PRICE': 15}})
+    'FILTER[%3E%3DPRICE]=15'
+
+    Экранирование значений
+    >>> convert_params({'FIELDS': {'POST_TITLE': "[1] + 1 == 11 // true"}})
+    'FIELDS[POST_TITLE]=%5B1%5D%20%2B%201%20%3D%3D%2011%20//%20true'
+
+    и т.д.
+    """
+
+    def recursive_traverse(values, key=None):
+
+        """
+        При первом вызове key ничему не равен, затем к нему будут добавляться новые ключи.
+        '' => 'field' => 'field[hello]' => 'field[hello][there]' => ...
+
+        Если values - строка, то возвращается строка вида 'key=values', иначе в список собираются такие же ключи и
+        собираются в строку вида 'key=value&key=value' и т.д.
+        """
+
+        collection_t = dict, list, tuple,
+        list_like_t = list, tuple,
+
+        params = []
+
+        if not isinstance(values, collection_t):
+            # Скалярное значение
+            values = '' if values is None else values
+
+            if not isinstance(values, RawStringParam):
+                # convert int, float, lazy_str to str
+                pass
+            else:
+                values = str(values)
+
+            return u'%s=%s' % (key, values)
+
+        if key is not None and isinstance(values, collection_t) and not values:
+            # Для некоторых методов обязательно указывать пустые параметры,
+            # например https://dev.1c-bitrix.ru/rest_help/tasks/task/item/list.php
+            # Из доков:
+            #     Однако, если какие-то параметры нужно пропустить,
+            #     то их все равно нужно передать, но в виде пустых массивов:
+            #     ORDER[]=&FILTER[]=&PARAMS[]=&SELECT[]=
+            return u'%s[]=' % key
+
+        # Создание итератора ключей и значений словаря/списка/кортежа
+        # (для списка и кортежа ключами становятся индексы)
+        if isinstance(values, list_like_t):
+            iterable = enumerate(values)
+        elif isinstance(values, dict):
+            iterable = values.items()
+        else:
+            raise TypeError(values)
+
+        # Тут проиходит добавление вложенности ключам и рекурсивный вызов
+        for inner_key, v in iterable:
+
+            # Кодируется только вложенная часть ключа,
+            # т.к. внешняя бывает только при рекурсивном вызове и уже
+            # может содержать квадратные скобки, которые мы хотим сохранить
+            inner_key = urllib.parse.quote(force_str(inner_key))
+
+            if key is not None:
+                inner_key = u'%s[%s]' % (key, inner_key)
+
+            result = recursive_traverse(v, inner_key)
+            if isinstance(result, list):
+                params.append(u'&'.join(result))
+            else:
+                params.append(result)
+
+        return params
+
+    return u'&'.join(recursive_traverse(form_data))
+
+
+def api_call(domain, api_method, auth_token, params=None, webhook=False, timeout=DEFAULT_TIMEOUT):
+    """POST-запрос к Bitrix24 api
+
+    :param domain: Полный адрес домена (it-solution.bitrix24.ru)
+    :param api_method: Имя метода (task.item.add)
+    :param auth_token: Токен пользователя
+    :param params: Словарь параметров. Может содержать просто ключ-значение,
+        либо списки, кортежи или опять словари.
+        Все это может быть перемешано.
+    :param webhook: Метода вызывается через webhook, auth_token в этом случае:
+        "{bitrix_id пользователя}/{ключ webhook'а}"
+    :param timeout: По умолчанию 60 секунд, если нужно убрать таймаут,
+        можно передать None, хотя возможно лучше передать просто большое
+        значение, например 30 * 60 (полчаса)
+
+    :rtype: requests.Response
+    :returns: Объект ответа библиотеки requests
+    """
+
+    if not params:
+        params = {}
+
+    hook_key = ''
+    if webhook:
+        hook_key = '{}/'.format(auth_token)
+
+    else:
+        params['auth'] = auth_token
+
+    converted_params = convert_params(params).encode('utf-8')
+    url = 'https://{domain}/rest/{hook_key}{api_method}.json'.format(
+        domain=domain, hook_key=hook_key, api_method=api_method
+    )
+
+    response = call_with_retries(url, converted_params, timeout=timeout)
+
+    if api_method != 'batch':
+        try:
+            data = response.json()
+            operating = data['time']['operating']
+            if operating > 300:
+                pass
+        except:
+            pass
+
+    return response
